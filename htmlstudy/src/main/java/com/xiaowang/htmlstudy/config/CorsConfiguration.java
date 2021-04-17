6cf5451b065733f6f1b6f4366bbb2ef8e1698ded
xialichu <953371497@qq.com>
2021-04-17 21:58:57 +0800

    跨域解决

--- htmlstudy/src/main/java/com/xiaowang/htmlstudy/config/CorsConfiguration.java
+++ htmlstudy/src/main/java/com/xiaowang/htmlstudy/config/CorsConfiguration.java
@@ -5,23 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @Description 跨域配置
- * @Date 2019/1/27 13:31
- **/
 @Configuration
-public class CORSConfig {
-
+public class CorsConfiguration {
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedHeaders("*")
-                        .allowedMethods("*")
+                        .allowCredentials(false)
+                        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                         .allowedOrigins("*");
             }
         };
     }
+
 }
