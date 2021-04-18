5020d12d99adc894ca24bca8ba7e62af0e0823a8
xialichu <953371497@qq.com>
2021-04-18 11:28:04 +0800

    跨域解决

--- htmlstudy/src/main/java/com/xiaowang/htmlstudy/controller/impl/HtmlKnowledgeController.java
+++ htmlstudy/src/main/java/com/xiaowang/htmlstudy/controller/impl/HtmlKnowledgeController.java
@@ -8,7 +8,6 @@ import com.xiaowang.htmlstudy.service.HtmlKnowledgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/html")
 public class HtmlKnowledgeController implements HtmlKnowledgeControllerApi {
